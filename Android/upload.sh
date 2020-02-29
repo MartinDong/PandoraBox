9ccc4d3158477093bb9232cbc474c4576c6b0eef
董宏宇 <martindhy@gmail.com>
2020-02-29 23:28:54 +0800

    将所有的项目上传到自己的MAVEN仓库

--- Android/upload.sh
+++ Android/upload.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./gradlew :doraemonkit:clean  :doraemonkit:uploadArchives
+./gradlew :doraemonkit:clean              :doraemonkit:uploadArchives
+./gradlew :doraemonkit-leakcanary:clean   :doraemonkit-leakcanary:uploadArchives
+./gradlew :doraemonkit-no-op:clean        :doraemonkit-no-op:uploadArchives
+./gradlew :doraemonkit-plugin:clean       :doraemonkit-plugin:uploadArchives
+./gradlew :doraemonkit-weex:clean         :doraemonkit-weex:uploadArchives
+./gradlew :doraemonkit-weex-no-op:clean   :doraemonkit-weex-no-op:uploadArchives
